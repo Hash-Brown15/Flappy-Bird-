2e743e2676d7c205acd8395d5f407166452c8e9b
Hash-Brown15 <JackHeppell1@gmail.com>
2025-06-25 20:52:37 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -13,9 +13,39 @@ pygame.display.set_caption("Flappy Bird")
 
 ground_scroll = 0
 scroll_speed = 4
+flying = False
+game_over = False
 
 bg = pygame.image.load("img/bg.png")
 ground_img = pygame.image.load("img/ground.png")
+
+class Bird(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.images = []
+        self.index = 0
+        self.counter = 0
+        for num in range(1, 4):
+            img = pygame.image.load(f'img/bird{num}.png')
+            self.images.append(img)
+        self.image = self.images[self.index]
+        self.rect.center = [x, y]
+    
+    def update(self):
+
+        self.counter += 1
+        falp_cooldown = 5
+
+        if self.counter > flap_cooldown:
+            self.counter = 0
+            self.index += 2
+            if self.index >= len(self.images):
+                self.index = 0
+        self.image = self.images[self.index]
+
+bird_group = pygame.sprite.Group()
+flappy = Bird(100, int(screen_height / 2))
+bird_group.add(flappy)
 
 
 run = True
@@ -26,13 +56,21 @@ while run:
     screen.blit(bg,(0,0))
 
     screen.blit(ground_img, (ground_scroll, 768))
-    ground_scroll -= scroll_speed
-    if abs(ground_scroll) > 35:
-        ground_scroll = 0
+
+    if flappy.rect.bottom > 768:
+        game_over = True
+        flying = False
+
+    if game_over == False:
+        ground_scroll -= scroll_speed
+        if abs(ground_scroll) > 35:
+            ground_scroll = 0
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        if event.type == pygame.MOUSEBUTTONDOWN and flying == False and game_over == False:
+            flying = True
 
     pygame.display.update()
 
