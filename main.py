a3ce633d6088e896d32a2d9d146d03541b3c98c6
Hash-Brown15 <JackHeppell1@gmail.com>
2025-07-02 21:06:56 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -30,20 +30,57 @@ class Bird(pygame.sprite.Sprite):
             self.images.append(img)
         self.image = self.images[self.index]
         self.rect.center = [x, y]
+        self.vel = 0
+        self.clicked = False
     
     def update(self):
 
-        self.counter += 1
-        falp_cooldown = 5
+        if flying == True:
+            self.vel += 0.5
+            if self.vel > 8:
+                self.vel = 8
+            if self.rect.bottom < 768:
+                self.rect.y += int(self.vel)
 
-        if self.counter > flap_cooldown:
-            self.counter = 0
-            self.index += 2
-            if self.index >= len(self.images):
-                self.index = 0
-        self.image = self.images[self.index]
+        if game_over == False:
+            if pugame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
+                self.vel = -10
+            if pygame.mouse.get_pressed()[0] == 0:
+                self.clicked = False
 
+            self.counter += 1
+            flap_cooldown = 5
+
+            if self.counter > flap_cooldown:
+                self.counter = 0
+                self.index += 1
+                if self.index >= len(self.images):
+                    self.index = 0
+            self.image = self.images[self.index]
+
+            self.image = pygame.transform.rotate(self.images[self.index])
+        else:
+            self.image = pygame.transform.rotate(self.images[self.index])
+    
+class Pipe(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('img/pipe.png')
+        self.rect = pygame.image,load('img/pipe.png')
+        self.rect = self.image.get_rect()
+        if position == 1:
+            self.image = pygame.transform.flip(self.image, False, True)
+            self.rect.bottomleft = [x, y - int(pipe_gap / 2)]
+        if position == -1:
+            self.rect.topleft = [x, y + int(pipe_gap / 2)]
+    
+    def update(self):
+        self.rect.x -= scroll_speed
+        if self.rect.right < 0:
+            self.kill()
 bird_group = pygame.sprite.Group()
+pipe_group = pygame.sprite.Group()
 flappy = Bird(100, int(screen_height / 2))
 bird_group.add(flappy)
 
@@ -55,6 +92,12 @@ while run:
 
     screen.blit(bg,(0,0))
 
+    bird_group.draw(screen)
+    bird_group.update()
+    pipe_group.draw(screen)
+
+    if pygame.sprite.groupcollide(bird_group, pipe_group, False, False) or
+        game_over
     screen.blit(ground_img, (ground_scroll, 768))
 
     if flappy.rect.bottom > 768:
@@ -62,9 +105,21 @@ while run:
         flying = False
 
     if game_over == False:
+
+        time_now = pygame.time.get_ticks()
+        if time_now - last_pipe > pipe_frequency:
+            pipe_height = random.randint(-100, 100)
+            btm_pipe = Pipe(screen_width, int(screen_height / 2) + pipe_height)
+            top_pipe = Pipe(screen_width, int(screen_height / 2) + pipe_height, -1)
+            pipe_group.add(btm_pipe)
+            pipe_group.add(top_pipe)
+            last_pipe = time_now
+
         ground_scroll -= scroll_speed
         if abs(ground_scroll) > 35:
             ground_scroll = 0
+
+        pipe_group.update()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
